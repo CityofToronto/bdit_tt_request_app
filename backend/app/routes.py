2e887607cc70cf9e4cd830371d15d88827cbb2c8
Adrian Zhao <acceleratorsnow@gmail.com>
2020-10-24 14:37:33 -0400

    Some interfaces. Testing geojson display/formats.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,3 +4,14 @@ from app import app
 @app.route('/')
 def index():
     return "Data Filter Web Application"
+
+
+@app.route('/closest-node/<latitude>/<longitude>')
+def get_closest_node(latitude, longitude):
+    raise NotImplementedError
+
+
+@app.route('/find-link/<from_node_id>/<to_node_id>')
+def get_link_between_nodes(from_node_id, to_node_id):
+    raise NotImplementedError
+
