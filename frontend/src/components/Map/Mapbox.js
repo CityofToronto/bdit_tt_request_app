317ca3ed26d11a014b1a557ec464b6496b5a562f
KenSB <kennethsb.wong@gmail.com>
2020-10-24 19:00:52 -0400

    Implemented the backbone of the map

--- frontend/src/components/Map/Mapbox.js
+++ frontend/src/components/Map/Mapbox.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2Vuc2IiLCJhIjoiY2tnb2E5ODZvMDlwMjJzcWhyamt5dWYwbCJ9.2uVkSjgGczylf1cmXdY9xQ';
 
 class Mapbox extends React.Component {
-  state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 2
+    };
+  }
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
   }
   render() {
     return (
-      <div/>
+      <div>
+        <div  ref={el => this.mapContainer = el} className="mapContainer"/>
+      </div>
     );
   }
 }
