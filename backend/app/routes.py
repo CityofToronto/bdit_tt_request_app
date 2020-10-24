330f8d3d660209eec3e1a8e9c75472d8e31712a9
Adrian Zhao <acceleratorsnow@gmail.com>
2020-10-24 14:56:14 -0400

    Added more API interfaces.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,17 +1,21 @@
 from app import app
 
 
-@app.route('/')
-def index():
-    return "Data Filter Web Application"
-
-
-@app.route('/closest-node/<latitude>/<longitude>')
+@app.route('/closest-node/<latitude>/<longitude>', methods=['GET'])
 def get_closest_node(latitude, longitude):
     raise NotImplementedError
 
 
-@app.route('/find-link/<from_node_id>/<to_node_id>')
-def get_link_between_nodes(from_node_id, to_node_id):
+@app.route('/link-nodes/<from_node_id>/<to_node_id>', methods=['GET'])
+def get_links_between_nodes(from_node_id, to_node_id):
     raise NotImplementedError
 
+
+@app.route('/travel-data', methods=['POST'])
+def get_links_travel_data():
+    raise NotImplementedError
+
+
+@app.route('/date-bounds', methods=['GET'])
+def get_date_bounds():
+    raise NotImplementedError
