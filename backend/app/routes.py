e5306134da98316edf049d273b7b18db3f6e2f88
Adrian Zhao <acceleratorsnow@gmail.com>
2020-10-24 19:38:40 -0400

    Integrated Flask Pytest. Made API document.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
 
 
+@app.route('/')
+def index():
+    return "Data Filter Web Application"
+
+
 @app.route('/closest-node/<latitude>/<longitude>', methods=['GET'])
 def get_closest_node(latitude, longitude):
     raise NotImplementedError
