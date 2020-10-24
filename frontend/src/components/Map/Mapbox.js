a9254b0493211a775dae3e930114c540bd53e6f0
Qiwen Liu <qiwen.liu@mail.utoronto.ca>
2020-10-24 19:55:58 -0400

    fixed the location of the map

--- frontend/src/components/Map/Mapbox.js
+++ frontend/src/components/Map/Mapbox.js
@@ -1,44 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2Vuc2IiLCJhIjoiY2tnb2E5ODZvMDlwMjJzcWhyamt5dWYwbCJ9.2uVkSjgGczylf1cmXdY9xQ';
 
 class Mapbox extends React.Component {
   constructor(props) {
-    super(props);
+  super(props);
     this.state = {
-    lng: -79.3957,
-    lat: 43.6629,
-    zoom: 15
+      lng: -79.3957,
+      lat: 43.6629,
+      zoom: 15
     };
-    }
-     
-    componentDidMount() {
+  }
+
+  componentDidMount() {
     const map = new mapboxgl.Map({
-    container: this.mapContainer,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [this.state.lng, this.state.lat],
-    zoom: this.state.zoom
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
     });
-     
+
     map.on('move', () => {
-    this.setState({
-    lng: map.getCenter().lng.toFixed(4),
-    lat: map.getCenter().lat.toFixed(4),
-    zoom: map.getZoom().toFixed(2)
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
     });
-    });
-    }
-     
-    render() {
+  }
+
+  render() {
     return (
-    <div>
-    <div className='sidebarStyle'>
-    <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
-    </div>
-    <div ref={el => this.mapContainer = el} className='mapContainer' />
-    </div>
+      <div>
+        <div className='sidebarStyle'>
+          <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+        </div>
+        <div ref={el => this.mapContainer = el} className='mapContainer' />
+      </div>
     )
-    }
+  }
 }
+
 export default Mapbox;
