f1a257025c8ed94c622a7926673bfee37b9caf28
Altair59 <39001328+Altair59@users.noreply.github.com>
2020-10-24 21:40:26 -0400

    created actions dir

---
+++ frontend/src/actions/actions.js
@@ -0,0 +1,4 @@
+const axios = require('axios');
+axios.defaults.withCredentials = true;
+/* specify domain addr */
+const domain = "http://backendtest-env.eba-aje3qmym.ca-central-1.elasticbeanstalk.com/;
