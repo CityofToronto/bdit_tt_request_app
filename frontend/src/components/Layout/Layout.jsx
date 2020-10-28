6fc2885be65bee473b38ca3c595b2bd985216f08
Daniel <daniel.jarvis@mail.utoronto.ca>
2020-10-27 22:52:59 -0400

    Added in a sidebar that can be opened and closed.

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,10 +1,40 @@
 import React from "react";
-import Mapbox from "../Map/Mapbox.js"
+import MapElement from "./Map";
+import Sidebar from "react-sidebar";
 
 class Layout extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            sidebarOpen: true
+        };
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this)
+    }
+
+    onSetSidebarOpen(open){
+        this.setState({ sidebarOpen: open });
+    }
+
     render(){
 
-        return <Mapbox/>
+        return (
+            <div id={"header"} style={{color: "black"}}>
+                <Sidebar
+                    sidebar={<h1>This is the content on the sidebar</h1>}
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+                    styles={{sidebar: {background: "white", width: 300}}}
+                >
+                    <button
+                        onClick={()=> this.onSetSidebarOpen(true)}
+                        style={{position: "absolute", right: "5%", height: "40px", width: "10%", top: "5px"}}
+                    >
+                        Open Sidebar
+                    </button>
+                </Sidebar>
+                <MapElement/>
+            </div>
+        );
 
     }
 }
