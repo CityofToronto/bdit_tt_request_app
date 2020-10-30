1443edb046ee8d02df162b1fe8508c44663aff66
Adrian Zhao <acceleratorsnow@gmail.com>
2020-10-30 09:25:15 -0400

    Trying to use testing.postgresql to mock psql instance.

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
 from app import app
+from sqlalchemy import create_engine
+import testing.postgresql
 
 
 @pytest.fixture
@@ -16,3 +18,14 @@ def db():
     from app import db
 
     yield db
+
+
+@pytest.fixture
+def mock_db():
+    with testing.postgresql.Postgresql(port=7654) as psql:
+        engine = create_engine(psql.url())
+
+        import pg8000
+        mock_db = pg8000.connect(psql.dsn())
+
+        yield mock_db, engine
--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -1,7 +1,7 @@
 from app.models import Node, Link, Travel
 
 
-def test_database_connection(client, db):
+def test_real_database_connection(client, db, mock_db):
     """Test if the connection is live and the application can read from remote database."""
     node_table = db.session.query(Node).first()
     link_table = db.session.query(Link).first()
@@ -10,3 +10,10 @@ def test_database_connection(client, db):
     assert link_table is not None
     assert node_table is not None
     assert travel_table is not None
+
+
+def test_mock_database_connection(client, db, mock_db):
+    mock = mock_db[0]
+    mock_eng = mock_db[1]
+
+    assert db.session.query(Link).first() is None
