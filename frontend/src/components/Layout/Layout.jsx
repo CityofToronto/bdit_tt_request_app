a5a06adfcd34de161886d0a74efc6c24ec8dd554
Daniel <daniel.jarvis@mail.utoronto.ca>
2020-11-07 13:04:54 -0500

    Moved parameters from sidebar to layout.

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -10,7 +10,11 @@ class Layout extends React.Component{
         this.state={
             sidebarOpen: true,
             nodesList: [],
-            linksList: []
+            linksList: [],
+            timeRange: "",
+            dateRange: "",
+            dayRange: "",
+            includeHolidays: false
         };
         this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this)
     }
@@ -35,11 +39,107 @@ class Layout extends React.Component{
         getLinksBetweenNodes(doc, this.state.nodesList)
     }
 
+    handleHolidays = () =>{
+        this.setState({ includeHolidays: !this.state.includeHolidays })
+    }
+
+    updatePreset = (event) => {
+        console.log(event);
+        switch(event.value){
+            case "Working Week":
+                this.setState({ timeRange: "0600:0900", dayRange: "mon:fri", includeHolidays: false });
+                break;
+
+            default:
+                this.setState({ timeRange: "", dayRange: "", includeHolidays: false })
+        }
+    }
+
+    updateDates = (e) => {
+        this.setState({dateRange: e.target.value})
+    }
+
+    updateTimes = (e) => {
+        this.setState({timeRange: e.target.value})
+    }
+
+    updateDays = (e) => {
+        this.setState({dayRange: e.target.value})
+    }
+
+    downloadData = () => {
+        let params = this.parseData()
+    }
+
+    parseData(){
+
+        let params = {};
+        let times = this.state.timeRange.split(":");
+        let days = this.state.dayRange.split(":");
+        const validDays = ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"];
+        let dates = this.state.dateRange.split(":");
+
+        // First validate the times
+        if(!(times.length === 2 && times[0].length === 4 && times[1].length === 4 &&
+            parseInt(times[0]) < 2400 && parseInt(times[1]) < 2400)){
+            alert("Please enter a time range following the format: hhmm:hhmm")
+
+            // next validate the days
+        } else if(!(days.length === 2 && validDays.includes(days[0].toLowerCase()) &&
+            validDays.includes(days[1].toLowerCase()))){
+            alert("Please enter a day range following the format: start:end")
+
+            // finally validate the dates
+        } else if(!(dates.length === 2 && this.validDate(dates[0]) && this.validDate(dates[1]))){
+            alert("Please enter a date range following the format: yyyy-mm-dd:yyyy-mm-dd");
+
+            // everything is valid so parse the data
+        } else {
+            params = {
+                startTimeStamp: dates[0] + " " + times[0],
+                endTimeStamp:dates[1] + " " + times[1],
+                startDay: days[0],
+                endDay: days[1]
+            }
+        }
+        return params;
+    }
+
+    validDate(dateString){
+        let segments = dateString.split("-");
+        if(segments.length === 3 && this.strIsInt(segments[0]) && segments[0].length === 4 &&
+            this.strIsInt(segments[1]) && segments[1].length === 2 &&
+            this.strIsInt(segments[2]) && segments[2].length === 2){
+            return true;
+        } else {
+            console.log(segments.length);
+            return false;
+        }
+    }
+
+    // From stack overflow
+    strIsInt(str){
+        str = str.replace(/^0+/, "") || "0";
+        let n = Math.floor(Number(str));
+        return n !== Infinity && String(n) === str && n >= 0;
+    }
+
     render(){
         return (
             <div id={"header"} style={{color: "black"}}>
                 <Sidebar
-                    sidebar={<SidebarContent/>}
+                    sidebar={<SidebarContent
+                        onHolidayUpdate={this.handleHolidays()}
+                        includeHolidays={this.state.includeHolidays}
+                        onDaysUpdate={this.updateDays()}
+                        dayRange={this.state.dayRange}
+                        onDatesUpdate={this.updateDates}
+                        dateRange={this.state.dateRange}
+                        onTimesUpdate={this.updateTimes()}
+                        presets={["Working Week", "Custom"]}
+                        onPresetChange={this.updatePreset()}
+                        onGo={this.downloadData}
+                    />}
                     open={this.state.sidebarOpen}
                     onSetOpen={this.onSetSidebarOpen}
                     styles={{sidebar: {background: "white", width: 300}}}
--- frontend/src/components/Layout/SidebarContent.jsx
+++ frontend/src/components/Layout/SidebarContent.jsx
@@ -6,97 +6,15 @@ import "react-dropdown/style.css";
 import "./SidebarContent.css";
 
 class SidebarContent extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            timeRange: "",
-            dateRange: "",
-            dayRange: "",
-            includeHolidays: false
-        }
-    }
-
-    updatePreset = (event) => {
-        console.log(event);
-        switch(event.value){
-            case "Working Week":
-                this.setState({ timeRange: "0600:0900", dayRange: "mon:fri", includeHolidays: false });
-                break;
-
-            default:
-                this.setState({ timeRange: "", dayRange: "", includeHolidays: false })
-        }
-    }
-
-    handleQuery = () =>{
-        let params = this.parseData()
-    }
-
-    parseData(){
-
-        let params = {};
-        let times = this.state.timeRange.split(":");
-        let days = this.state.dayRange.split(":");
-        const validDays = ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"];
-        let dates = this.state.dateRange.split(":");
-
-        // First validate the times
-        if(!(times.length === 2 && times[0].length === 4 && times[1].length === 4 &&
-            parseInt(times[0]) < 2400 && parseInt(times[1]) < 2400)){
-            alert("Please enter a time range following the format: hhmm:hhmm")
-
-            // next validate the days
-        } else if(!(days.length === 2 && validDays.includes(days[0].toLowerCase()) &&
-            validDays.includes(days[1].toLowerCase()))){
-            alert("Please enter a day range following the format: start:end")
-
-            // finally validate the dates
-        } else if(!(dates.length === 2 && this.validDate(dates[0]) && this.validDate(dates[1]))){
-            alert("Please enter a date range following the format: yyyy-mm-dd:yyyy-mm-dd");
-
-            // everything is valid so parse the data
-        } else {
-            params = {
-                startTimeStamp: dates[0] + " " + times[0],
-                endTimeStamp:dates[1] + " " + times[1],
-                startDay: days[0],
-                endDay: days[1]
-            }
-        }
-        return params;
-    }
-
-    validDate(dateString){
-        let segments = dateString.split("-");
-        if(segments.length === 3 && this.strIsInt(segments[0]) && segments[0].length === 4 &&
-            this.strIsInt(segments[1]) && segments[1].length === 2 &&
-            this.strIsInt(segments[2]) && segments[2].length === 2){
-            return true;
-        } else {
-            console.log(segments.length);
-            return false;
-        }
-    }
-
-    // From stack overflow
-    strIsInt(str){
-        str = str.replace(/^0+/, "") || "0";
-        let n = Math.floor(Number(str));
-        return n !== Infinity && String(n) === str && n >= 0;
-    }
 
     render(){
-        const handleChange = () => {
-            this.setState({includeHolidays: !this.state.includeHolidays})
-        };
 
-        const options = ["Working Week", "Custom"];
         return (
           <div id={"contentContainer"} className={"contentContainer"}>
               <Dropdown
-                  options={options}
+                  options={this.props.presets}
                   placeholder={"Presets"}
-                  onChange={this.updatePreset}
+                  onChange={this.props.onPresetChange}
                   className={"presets"}
                   id={"presets"}
               />
@@ -104,35 +22,29 @@ class SidebarContent extends React.Component{
                   label="time"
                   id="time"
                   className={"time"}
-                  onChange={(e) => {
-                      this.setState({timeRange: e.target.value})
-                  }}
-                  value={this.state.timeRange}
+                  onChange={this.props.onTimesUpdate}
+                  value={this.props.timeRange}
                   helperText={"e.g. 0600:0900"}
               />
               <TextField
                   label="dates"
                   id="date"
                   className={"date"}
-                  onChange={(e) => {
-                      this.setState({dateRange: e.target.value})
-                  }}
-                  value={this.state.dateRange}
+                  onChange={this.props.onDatesUpdate}
+                  value={this.props.dateRange}
                   helperText={"e.g. 2020-01-01:2020-0201"}
               />
               <TextField
                   label="days"
                   id="days"
                   className={"days"}
-                  onChange={(e) => {
-                      this.setState({dayRange: e.target.value})
-                  }}
+                  onChange={this.props.onDaysUpdate}
                   helperText={"e.g. mon:fri"}
-                  value={this.state.dayRange}
+                  value={this.props.dayRange}
               />
               <Checkbox
-                  checked={this.state.includeHolidays}
-                  onClick={handleChange}
+                  checked={this.props.includeHolidays}
+                  onClick={this.props.onHolidayUpdate}
                   inputProps={{ 'aria-label': 'primary checkbox' }}
                   id={"holiday"}
                   className={"holiday"}
@@ -141,7 +53,7 @@ class SidebarContent extends React.Component{
               <button
                   id={"go"}
                   className={"go"}
-                  onClick={this.handleQuery}
+                  onClick={this.props.onGo}
               >GO</button>
           </div>
         );
