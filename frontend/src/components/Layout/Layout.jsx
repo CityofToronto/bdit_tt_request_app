35e771e7ac192d9adcc343767fac123780faa3d0
Daniel <daniel.jarvis@mail.utoronto.ca>
2020-11-15 19:29:05 -0500

    deleted old commented out code from the prior implementation of the sidebar.

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -24,9 +24,6 @@ class Layout extends React.Component {
             sidebarOpen: true,
             nodesList: [],
             linksList: [],
-            // timeRange: "",
-            // dateRange: "",
-            // dayRange: "",
             startDate: MIN_DATE,
             endDate: MAX_DATE,
             startTime: MIN_DATE,
@@ -72,18 +69,6 @@ class Layout extends React.Component {
                 this.setState({timeRange: "", dayRange: "", includeHolidays: false})
         }
     }
-    //
-    // updateDates = (e) => {
-    //     this.setState({dateRange: e.target.value})
-    // }
-    //
-    // updateTimes = (e) => {
-    //     this.setState({timeRange: e.target.value})
-    // }
-    //
-    // updateDays = (e) => {
-    //     this.setState({dayRange: e.target.value})
-    // }
 
     onStartDateChange = (value) => {
         this.setState({startDate: value})
@@ -162,67 +147,7 @@ class Layout extends React.Component {
             "daysOfWeek": this.state.daysOfWeek,
             "includeHolidays": this.state.includeHolidays
         }
-        // let params = {};
-        // let times = this.state.timeRange.split(":");
-        // let days = this.state.dayRange.split(":");
-        // const validDays = ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"];
-        // let dates = this.state.dateRange.split(":");
-        //
-        // // First validate the times
-        // if (!(times.length === 2 && times[0].length === 4 && times[1].length === 4 &&
-        //     parseInt(times[0]) < 2400 && parseInt(times[1]) < 2400 &&
-        //     parseInt(times[0]) < parseInt(times[1]))) {
-        //
-        //     alert("Please enter a time range following the format: hhmm:hhmm")
-        //
-        //     // next validate the days
-        // } else if (!(days.length === 2 && validDays.includes(days[0].toLowerCase()) &&
-        //     validDays.includes(days[1].toLowerCase()) &&
-        //     validDays.indexOf(days[0].toLowerCase()) <= validDays.indexOf(days[1].toLowerCase()))) {
-        //
-        //     alert("Please enter a day range following the format: start:end")
-        //
-        //     // finally validate the dates
-        // } else if (!(dates.length === 2 && this.validDate(dates[0]) && this.validDate(dates[1]) &&
-        //     parseInt(dates[0].replaceAll("-", "")) <=
-        //     parseInt(dates[1].replaceAll("-", "")))) {
-        //
-        //     alert("Please enter a date range following the format: yyyy-mm-dd:yyyy-mm-dd");
-        //
-        //     // everything is valid so parse the data
-        // } else {
-        //     times[0] = times[0].substring(0, 2) + ":" + times[0].substring(2) + ":00"
-        //     times[1] = times[1].substring(0, 2) + ":" + times[1].substring(2) + ":00"
-        //     params = {
-        //         startTime: dates[0] + " " + times[0],
-        //         endTime: dates[1] + " " + times[1],
-        //         startDay: days[0],
-        //         endDay: days[1],
-        //         fileType: "csv"
-        //     }
-        // }
-        // return params;
     }
-
-    //
-    // validDate(dateString) {
-    //     let segments = dateString.split("-");
-    //     if (segments.length === 3 && this.strIsInt(segments[0]) && segments[0].length === 4 &&
-    //         this.strIsInt(segments[1]) && segments[1].length === 2 &&
-    //         this.strIsInt(segments[2]) && segments[2].length === 2) {
-    //         return true;
-    //     } else {
-    //         console.log(segments.length);
-    //         return false;
-    //     }
-    // }
-    //
-    // // From stack overflow
-    // strIsInt(str) {
-    //     str = str.replace(/^0+/, "") || "0";
-    //     let n = Math.floor(Number(str));
-    //     return n !== Infinity && String(n) === str && n >= 0;
-    // }
 
     render() {
         return (
@@ -231,12 +156,6 @@ class Layout extends React.Component {
                     sidebar={<SidebarContent
                         onHolidayUpdate={this.handleHolidays}
                         includeHolidays={this.state.includeHolidays}
-                        // onDaysUpdate={this.updateDays}
-                        // dayRange={this.state.dayRange}
-                        // onDatesUpdate={this.updateDates}
-                        // dateRange={this.state.dateRange}
-                        // onTimesUpdate={this.updateTimes}
-                        // presets={["Working Week", "Custom"]}
                         onPresetChange={this.updatePreset}
                         onGo={this.downloadData}
                         onStartDateChange={this.onStartDateChange}
