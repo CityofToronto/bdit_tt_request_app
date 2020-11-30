64a2f08cf477d2051c48383b607319ac6e1e17ac
Altair59 <39001328+Altair59@users.noreply.github.com>
2020-11-29 22:02:25 -0500

    changed back to using remote server

--- frontend/src/actions/actions.js
+++ frontend/src/actions/actions.js
@@ -3,8 +3,8 @@ import Axios from "axios";
 const axios = require('axios');
 axios.defaults.withCredentials = true;
 /* remote domain and local test domain */
-// const domain = "http://backendtest-env.eba-aje3qmym.ca-central-1.elasticbeanstalk.com";
-const domain = "http://127.0.0.1:5000";
+const domain = "http://backendtest-env.eba-aje3qmym.ca-central-1.elasticbeanstalk.com";
+//const domain = "http://127.0.0.1:5000";
 const fileDownload = require('js-file-download');
 
 const handleResponseError = (err) => {
