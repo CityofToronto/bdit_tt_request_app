ab22d4e0b67bcd95677733435ef4dbdf3f68346d
Adrian Zhao <acceleratorsnow@gmail.com>
2020-12-06 17:31:50 -0500

    Re-added POSTGIS_GEOM_SRID as it is required by the database models.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,6 +18,7 @@ TIMEZONE = timezone('US/Eastern')
 FULL_DATE_TIME_FORMAT = "%Y-%m-%d %H:%M"
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%H:%M"
+POSTGIS_GEOM_SRID = int(environ["POSTGIS_GEOM_SRID"])
 ALLOWED_FILE_TYPES = ['csv', 'xlsx']
 DB_START_DATE = datetime.strptime(environ['DB_DATA_START_DATE'], FULL_DATE_TIME_FORMAT)
 DB_TRAVEL_DATA_QUERY_RESULT_FORMAT = {
