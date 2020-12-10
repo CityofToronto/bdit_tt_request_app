48308080ea008956d4262c5f072341f874c596fa
Adrian Zhao <acceleratorsnow@gmail.com>
2020-12-10 16:58:11 -0500

    Reflect changes to deletion of travel table

--- backend/app/tests/test_db.py
+++ backend/app/tests/test_db.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.models import Node, Link, Travel
+from app.models import Node, Link
 
 
 @pytest.fixture
@@ -14,8 +14,6 @@ def test_real_database_connection(db):
     """Test if the connection is live and the application can read from remote database."""
     node_table = db.session.query(Node).first()
     link_table = db.session.query(Link).first()
-    travel_table = db.session.query(Travel).first()
 
     assert link_table is not None
     assert node_table is not None
-    assert travel_table is not None
