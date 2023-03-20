acfd6972e8342b388b25384068e3d470a67a961c
Nate Wessel <nate.wessel@toronto.ca>
2023-03-20 16:40:42 -0400

    Use NODE_ENV for server URL config (#21)
    * configure server url per node env
    
    * remove logging

--- frontend/src/actions/actions.js
+++ frontend/src/actions/actions.js
@@ -4,8 +4,8 @@ import {VALID_COLUMN_NAMES} from "../components/Layout/FieldSelectMenu/FieldSele
 const axios = require('axios');
 axios.defaults.withCredentials = true;
 /* remote domain and local test domain */
-const domain = "https://10.160.2.198/tt-request-backend";
-// const domain = "http://127.0.0.1:5000";
+const domain = process.env.NODE_ENV === 'development' ? 
+    'http://127.0.0.1:5000' : 'https://10.160.2.198/tt-request-backend';
 const fileDownload = require('js-file-download');
 
 const handleResponseError = (err) => {
