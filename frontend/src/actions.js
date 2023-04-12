d51f4c811018eb4e07ce14ba96b1985857e51ccc
Nate Wessel <nate.wessel@toronto.ca>
2023-04-12 11:23:11 -0400

    fully switch to GET endpoint

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 axios.defaults.withCredentials = true;
 /* remote domain and local test domain */
 const domain = process.env.NODE_ENV === 'development' ? 
-    'http://127.0.0.1:5007' : 'https://10.160.2.198/tt-request-backend';
+    'http://127.0.0.1:5000' : 'https://10.160.2.198/tt-request-backend';
 const fileDownload = require('js-file-download');
 
 const handleResponseError = (err) => {
@@ -95,14 +95,17 @@ export const updateClosestNode = (page, data) => {
 export function getLinksBetweenNodes(map, sequences){
     sequences.forEach((nodes,seqIndex) => {
         const nodePairs = nodes.map( (node,i) => {
-            if ( i > 0 ) return { from: node.nodeId, to: nodes[i-1].nodeId }
+            if ( i > 0 ) return { from: nodes[i-1].nodeId, to: node.nodeId }
+            return undefined
         } ).filter(v=>v)
         Promise.all( nodePairs.map( pair => (
             axios.get(`${domain}/link-nodes/${pair.from}/${pair.to}`)
         ) ) ).then( responses => {
-            console.log(data)
-            if (data) {
-                map.displayLinks( data, seqIndex )
+            if ( responses.every( response => response.status === 200 ) ) {
+                map.displayLinks(
+                    responses.map( response => response.data ),
+                    seqIndex
+                )
             } else {
                 alert("Failed to fetch links between nodes")
             }
