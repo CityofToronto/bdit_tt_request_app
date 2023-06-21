a6d7c9657547fa031fe7fa6a134b321d15c6e67b
Nate Wessel <nate.wessel@toronto.ca>
2023-06-21 15:19:47 -0400

    remove second map??

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,6 @@
 import Layout from './components/Layout'
-import Map from "./components/Map"
 import './App.css'
 
 export default function App(){
-    return (
-        <>
-            <Map/>
-            <Layout/>
-        </>
-    )
+    return <Layout/>
 }
