43b43775af1d3a03dc77c1f4493bd66af12aeeb2
Nate Wessel <nate.wessel@toronto.ca>
2023-06-21 13:25:26 -0400

    Drop unused python packages from requirements (#53)
    * dropmost pip requirements
    
    * add setuptools

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,13 +4,11 @@ from flask_cors import CORS
 from config import Config
 from os import environ
 from datetime import datetime
-from pytz import timezone
 
 app = Flask(__name__)
 CORS(app=app, supports_credentials=True)
 app.config.from_object(Config)
 
-TIMEZONE = timezone('US/Eastern')
 FULL_DATE_TIME_FORMAT = "%Y-%m-%d %H:%M"
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%H:%M"
