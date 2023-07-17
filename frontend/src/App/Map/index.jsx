f305c948188b2f39d2c669fd6c27df31f33d5990
Nate Wessel <nate.wessel@toronto.ca>
2023-07-17 17:21:27 -0400

    figuring out how to use map events

--- frontend/src/App/Map/index.jsx
+++ frontend/src/App/Map/index.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import { icon } from 'leaflet'
 import standardIcon from 'leaflet/dist/images/marker-icon.png'
 import standardIconShadow from 'leaflet/dist/images/marker-shadow.png'
@@ -14,12 +14,18 @@ const center = { lat: 43.65720, lng: -79.34299 }
 export default function Map() {
     return (
         <MapContainer center={center} zoom={15} style={{height: '100vh'}}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-            <Marker position={center} icon={defaultIcon}>
-            <Popup>
-                A pretty CSS3 popup. <br/> Easily customizable.
-            </Popup>
-            </Marker>
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+            <Marker position={center} icon={defaultIcon}/>
+            <DataLayer/>
         </MapContainer>
     )
+}
+
+import { useMapEvent } from 'react-leaflet/hooks'
+
+function DataLayer(){
+    // I bet with a ref, I can compare to the event target to see what was clicked
+    const map = useMapEvent('click', (event) => {
+        console.log(event)
+    } )
 }
