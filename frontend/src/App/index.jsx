36a013f1e2cf27e8b720f929aabd8b7500e6a03d
Nate Wessel <nate.wessel@toronto.ca>
2023-07-19 12:51:38 -0400

    kill the legend box - Ill just do this all from the panel

--- frontend/src/App/index.jsx
+++ frontend/src/App/index.jsx
@@ -1,22 +1,26 @@
+import { useState } from 'react'
 import SidebarContent from "./Sidebar"
 import Map from "./Map"
 
 import './layout.css'
 
 export default function Layout(){
+    // state variable for forcing a rerender when necessary, 
+    // but also tracking user inputs which may be useful for debugging
+    // is just an array of strings
+    const [ activityLog, setActivityLog ] = useState([])
+    function logActivity(activityDescription){
+        console.log(activityDescription)
+        setActivityLog( [ ...activityLog, activityDescription ] )
+    }
     return (
         <div className='layoutContainer'>
             <div className='layoutSidebar'>
-                <h2>The sidebar will go here</h2>
-                {false && <SidebarContent/>}
+                <SidebarContent logActivity={logActivity}/>
             </div>
 
             <div className='layoutMap'>
                 <Map/>
-            </div>
-
-            <div className='layoutMapControls'>
-
             </div>
         </div>
     )
