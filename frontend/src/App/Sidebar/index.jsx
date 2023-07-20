5c5dcbcb6d6c2be60875edc8c780ffbb38829ea4
Nate Wessel <nate.wessel@toronto.ca>
2023-07-20 09:52:23 -0400

    add welcome message

--- frontend/src/App/Sidebar/index.jsx
+++ frontend/src/App/Sidebar/index.jsx
@@ -1,5 +1,6 @@
-import { spatialDataInstance as spatialData } from '../../spatialData.js' 
+import { spatialDataInstance as spatialData } from '../../spatialData.js'
 
+import './sidebar.css'
 
 export default function SidebarContent({logActivity}){
     function createCorridor(){
@@ -7,8 +8,23 @@ export default function SidebarContent({logActivity}){
         console.log(spatialData.activeCorridor)
         logActivity('create corridor')
     }
-    return ( <> 
-        <h2>Toronto Historic Travel Times</h2>
+    return ( <div className="sidebarContent">
+        <Welcome/>
         <button onClick={createCorridor}>Add a new corridor to the map</button>
+    </div> )
+}
+
+function Welcome(){
+    return ( <>
+        <h2>Toronto Historic Travel Times</h2>
+        <p>
+            This application allows you to query averaged motor vehicle travel
+            times across the city, as far back as 2012. Data come from a small
+            sample of probe vehicles that report their location data 
+            to <a href="https://www.here.com/">Here</a>. For more information on
+            this application and our methodology 
+            see <a href="https://github.com/CityofToronto/bdit_tt_request_app">
+            the documentation</a>. 
+        </p>
     </> )
 }
