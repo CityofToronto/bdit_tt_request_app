33e74b6983a58784ebd670a9d38e541f0d50667c
Nate-Wessel <nate.wessel@toronto.ca>
2024-03-26 13:53:03 +0000

    dont calculate seconds if travel time is null

--- frontend/src/travelTimeQuery.js
+++ frontend/src/travelTimeQuery.js
@@ -66,7 +66,10 @@ export class TravelTimeQuery {
         record.set('hoursInRange', this.hoursInRange)
         record.set('estimatedVehicleCount', this.#estimatedSample)
         record.set('mean_travel_time_minutes', this.#travelTime)
-        record.set('mean_travel_time_seconds', 60* this.#travelTime)
+        record.set(
+            'mean_travel_time_seconds',
+            this.#travelTime ? 60 * this.#travelTime : null
+        )
 
         if(type=='json'){
             return Object.fromEntries(record) // can't JSONify maps
