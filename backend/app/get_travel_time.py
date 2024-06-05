520467b99173fab1212ac401891292344b2db177
Nate-Wessel <nate.wessel@toronto.ca>
2024-06-05 15:10:00 +0000

    have python aggregate hourly bins rather than SQL
    This should produce the same result, but sets us up for more advanced stuff later

--- backend/app/get_travel_time.py
+++ backend/app/get_travel_time.py
@@ -2,6 +2,18 @@
 
 from app.db import getConnection
 from app.get_links import get_links
+import numpy
+
+# the way we currently do it
+def mean_daily_mean(obs):
+    # group the observations by date
+    dates = {}
+    for (dt,tt) in obs:
+        dates[dt] = [tt] if not dt in dates else dates[dt] + [tt]
+    # take the daily averages
+    daily_means = [ numpy.mean(times) for times in dates.values() ]
+    #average the days together
+    return numpy.mean(daily_means)
 
 def get_travel_time(start_node, end_node, start_time, end_time, start_date, end_date, include_holidays, dow_list):
     """Function for returning data from the aggregate-travel-times/ endpoint"""
@@ -12,41 +24,22 @@ def get_travel_time(start_node, end_node, start_time, end_time, start_date, end_
             SELECT 1 FROM ref.holiday WHERE cn.dt = holiday.dt
         )'''
 
-    agg_tt_query = f'''
-        -- Aggregate segments to corridor on a daily, hourly basis
-        WITH corridor_hourly_daily_agg AS (
-            SELECT
-                cn.dt,
-                cn.hr,
-                SUM(cn.unadjusted_tt) * %(length_m)s::numeric / SUM(cn.length_w_data) AS corr_hourly_daily_tt
-            FROM congestion.network_segments_daily AS cn
-            WHERE   
-                cn.segment_id::integer = ANY(%(seglist)s)
-                AND cn.hr <@ %(time_range)s::numrange
-                AND date_part('ISODOW', cn.dt)::integer = ANY(%(dow_list)s)
-                AND cn.dt <@ %(date_range)s::daterange 
-                {holiday_clause}
-            GROUP BY
-                cn.dt,
-                cn.hr
-            -- where corridor has at least 80pct of links with data
-            HAVING SUM(cn.length_w_data) >= %(length_m)s::numeric * 0.8 
-        ),
-
-        -- Average the hours selected into daily period level data
-        corridor_period_daily_avg_tt AS (
-            SELECT
-                dt,
-                AVG(corr_hourly_daily_tt) AS avg_corr_period_daily_tt
-            FROM corridor_hourly_daily_agg
-            GROUP BY
-                dt
-        )
-
-        -- Average all the days with data to get period level data for each date range
-        SELECT 
-            ROUND(AVG(avg_corr_period_daily_tt) / 60, 2) AS average_tt_min
-        FROM corridor_period_daily_avg_tt
+    hourly_tt_query = f'''
+        SELECT
+            dt,
+            SUM(cn.unadjusted_tt) * %(length_m)s::real / SUM(cn.length_w_data) AS tt
+        FROM congestion.network_segments_daily AS cn
+        WHERE
+            cn.segment_id::integer = ANY(%(seglist)s)
+            AND cn.hr <@ %(time_range)s::numrange
+            AND date_part('ISODOW', cn.dt)::integer = ANY(%(dow_list)s)
+            AND cn.dt <@ %(date_range)s::daterange
+            {holiday_clause}
+        GROUP BY
+            cn.dt,
+            cn.hr
+        -- where corridor has at least 80pct of links with data
+        HAVING SUM(cn.length_w_data) >= %(length_m)s::numeric * 0.8;
     '''
 
     links = get_links(start_node, end_node)
@@ -66,13 +59,26 @@ def get_travel_time(start_node, end_node, start_time, end_time, start_date, end_
     connection = getConnection()
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(agg_tt_query, query_params)
-            # travel_time may be null if there's insufficient data
-            travel_time, = cursor.fetchone()
+            # get the hourly travel times
+            cursor.execute(hourly_tt_query, query_params)
+            sample = cursor.fetchall()
 
     connection.close()
+
+    tt_hourly = [ tt for (dt,tt) in sample ]
+
+    if len(sample) < 1:
+        # no travel times or related info to return here
+        return {
+            'travel_time': None,
+            'links': links,
+            'query_params': query_params
+        }
+
+    tt_seconds = mean_daily_mean(sample)
+
     return {
-        'travel_time': None if travel_time is None else float(travel_time),
+        'travel_time': round(tt_seconds / 60, 2),
         'links': links,
         'query_params': query_params
     }
