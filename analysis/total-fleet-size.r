f3b154096cbbb9b168862342463d22959752a479
Nate-Wessel <nate.wessel@toronto.ca>
2024-07-24 18:17:44 +0000

    comment/document

--- analysis/total-fleet-size.r
+++ analysis/total-fleet-size.r
@@ -10,7 +10,9 @@ con <- DBI::dbConnect(
     password = rstudioapi::askForPassword("Database password")
 )
 
-# look at hourly binning
+# The idea here is to convert average speeds and lengths of links into *time*
+# The total amount of time spent traveling by vehicles in a given unit of time
+# just is the average total number of vehicles reporting their locations to Here
 tbl( con, in_schema('here','ta_path') ) %>%
     filter( dt == '2024-07-18' ) %>%
     mutate( hours = sample_size * ((length / 1000) / mean) ) %>%
