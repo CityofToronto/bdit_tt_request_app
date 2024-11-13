d8caffcd24adfb2f4f6f2203fc6733cc730c1ce8
Nate-Wessel <nate.wessel@toronto.ca>
2024-11-13 14:52:46 +0000

    fetch map versions that overlap the query dates

--- backend/app/get_travel_time.py
+++ backend/app/get_travel_time.py
@@ -26,8 +26,33 @@ def timeFormat(seconds):
         'clock': f'{math.floor(seconds/3600):02d}:{math.floor((seconds/60)%60):02d}:{round(seconds%60):02d}'
     }
 
+def selectMapVersion(start_date, end_date):
+    query = """
+    WITH coverage AS (
+        SELECT
+            street_version,
+            valid_range * daterange(%(start_date)s, %(end_date)s,'[)') AS overlap
+        FROM here.street_valid_range
+    )
+
+    SELECT
+        street_version,
+        UPPER(overlap) - LOWER(overlap) AS days_covered
+    FROM coverage
+    WHERE UPPER(overlap) - LOWER(overlap) IS NOT NULL
+    ORDER BY days_covered DESC NULLS LAST
+    """
+    connection = getConnection()
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query,{'start_date':start_date,'end_date':end_date})
+            print(cursor.fetchall())
+    connection.close()
+
 def get_travel_time(start_node, end_node, start_time, end_time, start_date, end_date, include_holidays, dow_list):
     """Function for returning data from the aggregate-travel-times/ endpoint"""
+
+    selectMapVersion(start_date, end_date)
 
     holiday_clause = ''
     if not include_holidays:
