f886a61e1d737eea2ffcd61bf37e64b002b5bfea
Nate-Wessel <nate.wessel@toronto.ca>
2024-11-26 16:04:49 +0000

    refine, document

--- backend/app/get_node.py
+++ backend/app/get_node.py
@@ -5,13 +5,15 @@ from app.db import getConnection
 
 SQL = '''
 SELECT
-    ST_AsGeoJSON(ST_Dump(here_nodes.geom)) AS geom,
+    ST_AsGeoJSON(
+        ST_GeometryN(here_nodes.geom, 1) -- necessary because currently stored as a multi-point
+    ) AS geom,
     array_agg(DISTINCT InitCap(streets.st_name)) FILTER (WHERE streets.st_name IS NOT NULL) AS street_names
 FROM here.routing_nodes_23_4 AS here_nodes
 JOIN here_gis.streets_att_23_4 AS streets USING (link_id)
-WHERE node_id = %(node_id)s
+WHERE here_nodes.node_id = %(node_id)s
 GROUP BY
-    node_id,
+    here_nodes.node_id,
     here_nodes.geom;
 '''
 
