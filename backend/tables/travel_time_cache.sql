bcf77ffcc29c2635b76a97f0762d1f36d1a25e2a
Nate Wessel <nate.wessel@toronto.ca>
2024-12-12 17:03:23 -0500

    store the whole results object
    trading space for speed in setting this up, for now

--- backend/tables/travel_time_cache.sql
+++ backend/tables/travel_time_cache.sql
@@ -1,7 +1,7 @@
 CREATE TABLE nwessel.cached_travel_times (
     uri_string text CHECK(uri_string ~ '^\/\d+\/\d+\/\d{1,2}\/\d{1,2}\/\d{4}-\d{2}-\d{2}\/\d{4}-\d{2}-\d{2}\/(true|false)\/[1-7]{1,7}$'),
     commit_hash text,
-    travel_time_seconds real NOT NULL CHECK(travel_time_seconds > 0),
+    results jsonb NOT NULL,
     PRIMARY KEY (uri_string, commit_hash)
 );
 
