37284edc9ddd96c14f697dee78aed8204eb4bcfc
Nate-Wessel <nate.wessel@toronto.ca>
2025-01-07 19:45:57 +0000

    get street names for `px`

--- backend/app/nodes/byID/px.py
+++ backend/app/nodes/byID/px.py
@@ -3,10 +3,19 @@ from app.db import getConnection
 
 SQL = '''
 SELECT
-    px,
-    ST_AsGeoJSON(geom) AS geojson
-FROM traffic.traffic_signal
-WHERE "centrelineId" = %(centreline_id)s;
+    tts.px,
+    ST_AsGeoJSON(tts.geom) AS geojson,
+    array_remove(
+        ARRAY[
+            InitCap(gts.main_street),
+            InitCap(gts.side1_street),
+            InitCap(gts.side2_street)
+        ],
+        NULL
+    ) AS street_names
+FROM traffic.traffic_signal AS tts
+JOIN gis.traffic_signal AS gts ON tts.px = gts.px::int
+WHERE tts."centrelineId" = %(centreline_id)s;
 '''
 
 def get_px_node(centreline_id):
@@ -16,10 +25,11 @@ def get_px_node(centreline_id):
             cursor.execute(SQL, {"centreline_id": centreline_id})
             if cursor.rowcount != 1:
                 return None
-            px, geojson = cursor.fetchone()
+            px, geojson, street_names = cursor.fetchone()
             node = {
                 'node_id': px,
                 'network': 'px',
+                'street_names': street_names,
                 'geometry': json.loads(geojson)
             }
     return node
