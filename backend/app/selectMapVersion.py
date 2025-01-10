217f82d44b6185ad0e114ff1b1e0f039ebbfb5ea
Nate-Wessel <nate.wessel@toronto.ca>
2025-01-10 17:14:05 +0000

    allow function to default to NOW

--- backend/app/selectMapVersion.py
+++ backend/app/selectMapVersion.py
@@ -3,26 +3,35 @@ from app.db import getConnection
 # Selects Here map version to use based on dates in the provided query.
 # however the query also has nodes which may or may not be version-specific
 
-def selectMapVersion(start_date, end_date):
-    query = """
-    WITH coverage AS (
-        SELECT
-            street_version,
-            valid_range * daterange(%(start_date)s, %(end_date)s,'[)') AS overlap
-        FROM here.street_valid_range
-    )
-
+rangeQuery = """
+WITH coverage AS (
     SELECT
         street_version,
-        UPPER(overlap) - LOWER(overlap) AS days_covered
-    FROM coverage
-    WHERE UPPER(overlap) - LOWER(overlap) IS NOT NULL
-    ORDER BY days_covered DESC NULLS LAST
-    """
+        valid_range * daterange(%(start_date)s, %(end_date)s,'[)') AS overlap
+    FROM here.street_valid_range
+)
+
+SELECT street_version
+FROM coverage
+WHERE UPPER(overlap) - LOWER(overlap) IS NOT NULL
+ORDER BY UPPER(overlap) - LOWER(overlap) DESC NULLS LAST;
+"""
+
+nowQuery = """
+SELECT street_version
+FROM here.street_valid_range
+WHERE valid_range @> NOW()::date;
+"""
+
+def selectMapVersion(start_date=None, end_date=None):
     connection = getConnection()
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(query,{'start_date':start_date,'end_date':end_date})
-            (map_version, coverage) = cursor.fetchone()
+            # a daterange has been provided
+            cursor.execute(
+                rangeQuery if start_date and end_date else nowQuery,
+                {'start_date':start_date,'end_date':end_date}
+            )
+            (map_version) = cursor.fetchone()
     connection.close()
     return map_version
