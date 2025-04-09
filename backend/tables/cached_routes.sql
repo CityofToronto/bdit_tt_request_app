65f5a434437fa8fe4af007b9e7407cb36655579e
Nate Wessel <nate.wessel@toronto.ca>
2025-04-09 15:53:54 -0400

    create storage table

---
+++ backend/tables/cached_routes.sql
@@ -0,0 +1,12 @@
+CREATE TABLE nwessel.cached_tt_routes (
+    node_start bigint NOT NULL CHECK (node_start >= 0),
+    node_end bigint NOT NULL CHECK (node_end >= 0),
+    map_version text NOT NULL, --REFERENCES here.street_valid_range (street_version),
+    commit_hash text NOT NULL,
+    results jsonb NOT NULL,
+    first_requested timestamptz DEFAULT NOW(),
+    PRIMARY KEY (node_start, node_end, map_version, commit_hash)
+);
+
+GRANT SELECT, INSERT ON nwessel.cached_tt_routes TO tt_request_bot;
+GRANT SELECT ON nwessel.cached_tt_routes TO bdit_humans;
