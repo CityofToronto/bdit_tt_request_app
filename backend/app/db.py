1e9ee022a3310cb7a93191ad13335078cf7f1c4a
Nate-Wessel <nate.wessel@toronto.ca>
2025-04-10 20:47:11 +0000

    use pool everywhere

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,17 +6,15 @@ from psycopg_pool import ConnectionPool
 
 load_dotenv()
 
-kwargs = {
-    'host': os.environ['DB_HOST'],
-    'dbname': os.environ['DB_NAME'],
-    'user': os.environ['DB_USER'],
-    'password': os.environ['DB_USER_PASSWORD']
-}
-
-pool = ConnectionPool(kwargs=kwargs,min_size=1,max_size=5,open=True)
+pool = ConnectionPool(
+    kwargs={
+        'host': os.environ['DB_HOST'],
+        'dbname': os.environ['DB_NAME'],
+        'user': os.environ['DB_USER'],
+        'password': os.environ['DB_USER_PASSWORD']
+    },
+    min_size=1,
+    max_size=5,
+    open=True
+)
 pool.wait()
-
-def getConnection():
-    with pool.connection() as conn:
-        #print(conn.execute('SELECT * FROM here.ta LIMIT 1'))
-        yield conn
