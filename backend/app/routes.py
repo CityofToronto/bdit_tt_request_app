c804255629a1bd6e19225611f8a7a87add5fe731
Nate-Wessel <nate.wessel@toronto.ca>
2025-04-10 18:58:50 +0000

    define endpoints as separate

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -57,8 +57,8 @@ def closest_node(meters, longitude, latitude):
 
 # test URL /node/here/30357505
 @app.route('/node/<node_id>', endpoint='generic') # will be deprecated
-@app.route('/node/here/<node_id>', endpoint='here')
-@app.route('/node/centreline/<node_id>', endpoint='centreline')
+@app.route('/node/here/<node_id>', endpoint='here-nodes')
+@app.route('/node/centreline/<node_id>', endpoint='centreline-nodes')
 def get_node(node_id):
     """Returns information about a given node in the either the Here or
     Centreline street networks.
@@ -78,7 +78,7 @@ def get_node(node_id):
     doConflation = False
     if request.args.get('doConflation') is not None:
         doConflation = True
-    if request.endpoint == 'centreline':
+    if request.endpoint == 'centreline-nodes':
         node = get_centreline_node(node_id, doConflation)
     else: # here network
         node = get_here_node(node_id, doConflation)
@@ -86,8 +86,9 @@ def get_node(node_id):
 
 # test URL /link-nodes/here/30421154/30421153
 #shell function - outputs json for use on frontend
-@app.route('/link-nodes/<network>/<from_node_id>/<to_node_id>')
-def get_here_links_between_two_nodes(network, from_node_id, to_node_id):
+@app.route('/link-nodes/here/<from_node_id>/<to_node_id>', endpoint='here-links')
+@app.route('/link-nodes/centreline/<from_node_id>/<to_node_id>', endpoint='centreline-links')
+def get_here_links_between_two_nodes(from_node_id, to_node_id):
     """Returns a list of links/edges defining the shortest path between two nodes.
 
     Each link has 
@@ -96,12 +97,11 @@ def get_here_links_between_two_nodes(network, from_node_id, to_node_id):
         * a length in meters
         * the name of the street
         * source and target nodes in the reference network
-    Routing is done in PostgreSQL using `here_gis.get_links_btwn_nodes_{map_version}`
 
     arguments:
-    network (str): reference network to use; either 'here' or 'centreline'
     from_node_id (int): origin node ID on the reference network
     to_node_id (int): destination node ID on the reference network
+    optional GET param map_version applies only to Here network
     """
     try:
         from_node_id = int(from_node_id)
@@ -112,18 +112,16 @@ def get_here_links_between_two_nodes(network, from_node_id, to_node_id):
     if from_node_id == to_node_id:
         return jsonify({'error': "Source node can not be the same as target node."}), 400
 
-    if network == 'here':
+    if request.endpoint == 'here-links':
         map_version = request.args.get('map_version')
         if map_version and re.fullmatch(r'^\d{2}_\d$', map_version):
             # TODO: can pass map versions that match the pattern but don't exist
-            # which will cause database errors
+            # which will expose database errors
             links = get_here_links(from_node_id,to_node_id,map_version)
         else:
             links = get_here_links(from_node_id,to_node_id)
-    elif network == 'centreline':
+    elif request.endpoint == 'centreline-links':
         links = get_centreline_links(from_node_id, to_node_id)
-    else:
-        return jsonify({'error': "Network should be one of ['here','centreline']"}), 400
 
     return jsonify({
         "source": from_node_id, 
