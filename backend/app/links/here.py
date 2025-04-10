39994180ba9ebebd056c2178fe9c136ea2b9382a
Nate-Wessel <nate.wessel@toronto.ca>
2025-04-10 13:52:38 +0000

    adapt cache check/insert to new table structure

--- backend/app/links/here.py
+++ backend/app/links/here.py
@@ -7,49 +7,34 @@ from app.getGitHash import getGitHash
 cacheQuery = '''
 SELECT results
 FROM nwessel.cached_tt_routes
-WHERE
-    node_start = %(node_start)s
-    AND node_end = %(node_end)s
-    AND map_version = %(map_version)s
-    AND commit_hash = %(hash)s;
+WHERE uri_string = %(uri)s AND commit_hash = %(hash)s;
 '''
 
 cacheInsert = '''
-INSERT INTO nwessel.cached_tt_routes (node_start, node_end, map_version, commit_hash, results)
-VALUES (%(node_start)s,%(node_end)s,%(map_version)s, %(hash)s, %(results)s)
+INSERT INTO nwessel.cached_tt_routes (uri_string, commit_hash, results)
+VALUES (%(uri)s, %(hash)s, %(results)s)
 '''
 
-def cacheAndReturn(obj,node_start,node_end,map_version):
+def cacheAndReturn(obj,uri):
     connection = getConnection()
     with connection:
         with connection.cursor() as cursor:
             try:
                 cursor.execute(
                     cacheInsert,
-                    {
-                        'node_start': node_start,
-                        'node_end': node_end,
-                        'map_version': map_version,
-                        'hash': getGitHash(),
-                        'results': json.dumps(obj)
-                    }
+                    {'uri': uri,'hash':getGitHash(),'results':json.dumps(obj)}
                 )
             finally:
                 return obj
 
-def checkCache(node_start, node_end, map_version):
+def checkCache(uri):
     connection = getConnection()
     with connection:
         with connection.cursor() as cursor:
             try: # try keeps this loosely coupled - no strict dependency
                 cursor.execute(
                     cacheQuery,
-                    {
-                        'node_start': node_start,
-                        'node_end': node_end,
-                        'map_version': map_version,
-                        'hash': getGitHash()
-                    }
+                    {'uri':uri,'hash':getGitHash()}
                 )
                 for (record,) in cursor: # will skip if no records
                     return record # there could only be one because of constraint
@@ -87,7 +72,8 @@ def get_here_links(from_node_id, to_node_id, map_version='??_?'):
         # defaults to whatever map version covers latest data
         map_version = selectMapVersion()
     if map_version != '??_?':
-        cachedLinks = checkCache(from_node_id, to_node_id, map_version)
+        URI = f'/link-nodes/here/{from_node_id}/{to_node_id}?map_version={map_version}'
+        cachedLinks = checkCache(URI)
         if cachedLinks:
             return cachedLinks
 
@@ -120,4 +106,4 @@ def get_here_links(from_node_id, to_node_id, map_version='??_?'):
             ]
 
     connection.close()
-    return cacheAndReturn(links,from_node_id,to_node_id,map_version)
+    return cacheAndReturn(links,URI)
