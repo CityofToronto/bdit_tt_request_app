fe74409fd38936f53e8deac7322cd6210b8c83c0
Nate-Wessel <nate.wessel@toronto.ca>
2025-09-29 19:59:11 +0000

    tidy up bootstrap function

--- backend/app/travel_times/bootstrap.py
+++ backend/app/travel_times/bootstrap.py
@@ -1,23 +1,31 @@
 from app.travel_times.daily_aggregation import mean_daily_mean
 from traveltimetools.utils import timeFormats
-
 import random
 import numpy
 
+# this is a bit low, but efficient
+resamples = 100
+
+# 95% confidence interval
+pctLower = 2.5
+pctUpper = 97.5
 
 def bootstrap(sample):
-    reported_intervals = None
-    if len(sample) > 1:
-        # bootstrap for synthetic sample distribution
-        sample_distribution = []
-        for i in range(0,100):
-            bootstrap_sample = random.choices( sample, k = len(sample) )
-            sample_distribution.append( mean_daily_mean(bootstrap_sample) )
-        p95lower, p95upper = numpy.percentile(sample_distribution, [2.5, 97.5])
-        reported_intervals = {
-            'p=0.95': {
-                'lower': timeFormats(p95lower,1),
-                'upper': timeFormats(p95upper,1)
-            }
+    """returns bootstrapped confidence intervals for the sample"""
+
+    if len(sample) == 0:
+        return None
+    sample_distribution = []
+    for i in range(0, resamples):
+        bootstrap_sample = random.choices( sample, k = len(sample) )
+        sample_distribution.append( mean_daily_mean(bootstrap_sample) )
+    lowerCI, upperCI = numpy.percentile(
+        sample_distribution,
+        [pctLower, pctUpper]
+    )
+    return {
+        'p=0.95': {
+            'lower': timeFormats(lowerCI,1),
+            'upper': timeFormats(upperCI,1)
         }
-    return reported_intervals
+    }
