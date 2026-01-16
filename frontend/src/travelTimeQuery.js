180db6d1cba8f71035186fd4492d98b784dfc252
Nate-Wessel <nate.wessel@toronto.ca>
2026-01-16 17:12:28 +0000

    return 95 & 5th percentiles using d3-array quantile function

--- frontend/src/travelTimeQuery.js
+++ frontend/src/travelTimeQuery.js
@@ -1,4 +1,5 @@
 import { domain } from './domain.js'
+import { quantile } from 'd3-array'
 
 export class TravelTimeQuery {
     #corridor
@@ -112,7 +113,21 @@ export class TravelTimeQuery {
         // turning these off in the frontend until they're ready for production
         //record.set('moe_lower_p95', this.#results?.confidence?.intervals?.['p=0.95']?.lower?.seconds)
         //record.set('moe_upper_p95', this.#results?.confidence?.intervals?.['p=0.95']?.upper?.seconds)
-
+        record.set('n', this.#results.observations.length)
+        record.set(
+            '95th percentile (seconds)',
+            quantile(
+                this.#results.observations.map(obs => obs.seconds),
+                0.95
+            )
+        )
+        record.set(
+            '5th percentile (seconds)',
+            quantile(
+                this.#results.observations.map(obs => obs.seconds),
+                0.05
+            )
+        )
         if(type=='json'){
             return Object.fromEntries(record) // can't JSONify maps
         }else if(type=='csv'){
