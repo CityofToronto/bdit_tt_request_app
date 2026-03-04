a28b491a2928aab5c4404709a75891b898b9e111
Nate-Wessel <nate.wessel@toronto.ca>
2026-03-04 15:00:20 +0000

    restructure the bootstrap and results object
    set up for any number of estimators now
    
    Is not yet coordinated with frontend

---
+++ backend/app/travel_times/estimateParameters.py
@@ -0,0 +1,69 @@
+from traveltimetools.utils import timeFormats
+from random import choices
+from numpy import percentile, quantile
+import polars
+
+# Q: Is this the ideal way to do this? 
+# A: It is the way we currently do it.
+def mean_daily_mean(observations):
+    """Takes a list of DynamicBins and returns
+    the *average* of the *daily averages*"""
+
+    return polars.DataFrame({
+        'date': [bin.singleDate for bin in observations],
+        'travelTime': [bin.travelTime for bin in observations]
+    }).group_by('date').agg(
+        polars.col('travelTime').mean().alias('travelTime')
+    ).select(
+        polars.col('travelTime').mean()
+    ).item()
+
+def median(observations):
+    return quantile(
+        [obs.travelTime for obs in observations],
+        0.5
+    )
+
+# this is a quite low, but quick
+resamples = 100
+
+# 95% confidence interval
+pctLower = 2.5
+pctUpper = 97.5
+
+functions = {
+    'mean': mean_daily_mean,
+    'median': median
+}
+
+def estimateParameters(sample):
+    """returns bootstrapped confidence intervals for the sample"""
+    if len(sample) == 0:
+        return None
+    data = {}
+    
+    for funcName, func in functions.items():
+        data[funcName] = {
+            'estimate': timeFormats(func(sample)),
+            'bootstrapDistribution': []
+        }
+
+    for i in range(0, resamples):
+        # resample with replacement
+        bootstrapSample = choices( sample, k = len(sample) )
+        # estimate functions from resampled distribution
+        for funcName, func in functions.items():
+            data[funcName]['bootstrapDistribution'].append(func(bootstrapSample))
+
+    for estimate in data.values():
+        lowerCI, upperCI = percentile(
+            estimate['bootstrapDistribution'],
+            [pctLower, pctUpper]
+        )
+        estimate['ci'] = {
+            'lower': timeFormats(lowerCI),
+            'upper': timeFormats(upperCI)
+        }
+        del estimate['bootstrapDistribution']
+
+    return data
--- backend/app/travel_times/get_travel_time.py
+++ backend/app/travel_times/get_travel_time.py
@@ -5,8 +5,7 @@ from app.links.here import get_here_links
 from app.hereMapVersions import selectMapVersions
 from traveltimetools.utils import timeFormats
 from app.travel_times.cache import checkCache, cacheAndReturn
-from app.travel_times.bootstrap import bootstrap
-from app.travel_times.measures import mean_daily_mean, median
+from app.travel_times.estimateParameters import estimateParameters
 from app.corridors.conflateMapVersions import corridorsAreTheSame
 from app.travel_times.dynamic_bins import createDynamicBins
 import polars
@@ -142,14 +141,7 @@ def get_travel_time(start_node, end_node, start_time, end_time, start_date, end_
 
     return cacheAndReturn({
         'results': {
-            'travel_time': {
-                'mean': timeFormats(mean_daily_mean(observations), 1),
-                'median': timeFormats(median(observations))
-            },
-            'confidence': {
-                'sample': len(observations),
-                'intervals': bootstrap(observations)
-            },
+            'estimates': estimateParameters(observations),
             'observations': [timeFormats(bin.travelTime,1) for bin in observations]
         },
         'query': {
