1f3173813a715569b477bd82c67c2120f6d4df41
Nate-Wessel <nate.wessel@toronto.ca>
2026-03-04 16:31:19 +0000

    restore 1 decimal precision on observations

--- backend/app/travel_times/get_travel_time.py
+++ backend/app/travel_times/get_travel_time.py
@@ -136,7 +136,7 @@ def get_travel_time(start_node, end_node, start_time, end_time, start_date, end_
     return cacheAndReturn({
         'results': {
             'estimates': estimateParameters(observations),
-            'observations': [timeFormats(bin.travelTime) for bin in observations]
+            'observations': [timeFormats(bin.travelTime, 1) for bin in observations]
         },
         'query': {
             'corridor': {
